7c0e34d862c34a1388f5bdeb1c4f7a7d92a25f16
RomanSaypin <roman9126117@gmail.com>
2023-06-07 15:12:15 -0700

    Задач выполнена!

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,33 +16,33 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
-//
-//
-//      List<User> users = userService.listUsers();
-//      for (User user : users) {
-//         System.out.println("Id = "+user.getId());
-//         System.out.println("First Name = "+user.getFirstName());
-//         System.out.println("Last Name = "+user.getLastName());
-//         System.out.println("Email = "+user.getEmail());
-//         System.out.println();
-//      }
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
 
       Car car1 = new Car("TestCar1", 1);
-//      Car car2 = new Car("TestCar2", 2);
-//      Car car3 = new Car("TestCar3", 3);
-//      User user1 = new User("TestUser1", "LastName1", "userT1@mail.ru");
-//      User user2 = new User("TestUser2", "LastName2", "userT2@mail.ru");
-//      User user3 = new User("TestUser3", "LastName3", "userT3@mail.ru");
-//      user1.setCar(car1);
-//      user2.setCar(car2);
-//      user3.setCar(car3);
-//      userService.add(user1);
-//      userService.add(user2);
-//      userService.add(user3);
+      Car car2 = new Car("TestCar2", 2);
+      Car car3 = new Car("TestCar3", 3);
+      User user1 = new User("TestUser1", "LastName1", "userT1@mail.ru");
+      User user2 = new User("TestUser2", "LastName2", "userT2@mail.ru");
+      User user3 = new User("TestUser3", "LastName3", "userT3@mail.ru");
+      user1.setCar(car1);
+      user2.setCar(car2);
+      user3.setCar(car3);
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
 
 
      userService.getUser(car1.getModel(), car1.getSeries());
